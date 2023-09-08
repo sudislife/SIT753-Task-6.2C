1aff787c854175c776873d911e8e3d6b64a714f8
Cheeky <sudaksh77@gmail.com>
2023-09-09 00:44:33 +1000

    Added module docstring

--- say_hello.py
+++ say_hello.py
@@ -1,4 +1,10 @@
 def say_hello():
+    """
+    This function prints a simple greeting message.
+    
+    Returns:
+        str: The greeting message.
+    """
     str1 = 'Hello World! :grinning_face_with_big_eyes:'
     print(str1)
     return str1
