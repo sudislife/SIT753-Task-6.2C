2b7ff5f2648048abffee7f5b1f92e3c92559a8c4
Cheeky <sudaksh77@gmail.com>
2023-09-09 00:23:28 +1000

    Made a variable and used that instead

--- say_hello.py
+++ say_hello.py
@@ -1,6 +1,7 @@
 def say_hello():
-    print('Hello World! :grinning_face_with_big_eyes:')
-    return 'Hello World! :grinning_face_with_big_eyes:'
+    str1 = 'Hello World! :grinning_face_with_big_eyes:'
+    print(str1)
+    return str1
 
 if __name__ == '__main__':
     say_hello()
